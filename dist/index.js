fd508d488e8cf42f77a22df1c06247061c3b6132
Arjun Sharma <arjunsharma6622@gmail.com>
2024-05-11 18:27:12 +0530

    added vercel json file, and added some precommit scripts in package json

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+    return res.send("Hello World");
+});
+app.listen(8000, () => {
+    console.log(`Server running on port ${PORT}`);
+});
