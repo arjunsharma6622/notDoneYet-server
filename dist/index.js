fbbdc9c1ac509f0b5d7ff006c43b00fd59d8945f
Arjun Sharma <arjunsharma6622@gmail.com>
2024-05-11 23:51:12 +0530

    added cors origins access

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,16 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const utils_1 = require("./utils/utils");
 const user_1 = __importDefault(require("./routes/user"));
-dotenv_1.default.config();
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+const corsOptions = {
+    origin: ["http://localhost:3000", "https://notdoneyet.vercel.app"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
+dotenv_1.default.config();
 const PORT = process.env.PORT || 8000;
 (0, utils_1.connectDB)();
 app.get("/", (_req, res) => {
--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,30 @@ import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import { connectDB } from "./utils/utils";
 import userRoutes from "./routes/user";
+import cors, { CorsOptions } from "cors";
 
+const app = express();
+
+const corsOptions: CorsOptions = {
+  origin: ["http://localhost:3000", "https://notdoneyet.vercel.app"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 dotenv.config();
-const app = express()
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-connectDB()
+connectDB();
 
-app.get("/", (_req : Request, res : Response) => {
-    return res.send("Hello World")
-})
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Hello World");
+});
 
-app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes);
 
 app.listen(8000, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
