0373627fc00eacf3084aaef6ba788d44ebf2e86d
Arjun Sharma <arjunsharma6622@gmail.com>
2024-05-12 01:03:59 +0530

    added few user routes

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_1 = require("../models/user");
 const router = express_1.default.Router();
+// get all users
 router.get("/", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const users = yield user_1.User.find();
@@ -25,6 +26,7 @@ router.get("/", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }));
+// get user by id
 router.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const userId = req.params.id;
@@ -33,6 +35,40 @@ router.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () 
             return res.status(404).json({ error: "User not found" });
         }
         res.status(200).json(user);
+    }
+    catch (err) {
+        console.error(`Error fetching users: ${err}`);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}));
+// get user following
+router.get("/following/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const userId = req.params.id;
+        const user = yield user_1.User.findById(userId).populate("following");
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const userFollowings = user.following;
+        res.status(200).json(userFollowings);
+    }
+    catch (err) {
+        console.error(`Error fetching users: ${err}`);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}));
+// get recommended users
+router.get("/recommended/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const userId = req.params.id;
+        const user = yield user_1.User.findById(userId).populate("following");
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const userFollowings = user.following;
+        userFollowings.push(userId);
+        const recommendedUsers = yield user_1.User.find({ _id: { $nin: userFollowings } });
+        res.status(200).json(recommendedUsers);
     }
     catch (err) {
         console.error(`Error fetching users: ${err}`);
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { User } from "../models/user";
 const router = express.Router();
 
+// get all users
 router.get("/", async (_req: Request, res: Response) => {
   try {
     const users = await User.find();
@@ -12,6 +13,7 @@ router.get("/", async (_req: Request, res: Response) => {
   }
 });
 
+// get user by id
 router.get("/:id", async (req: Request, res: Response) => {
   try {
     const userId = req.params.id;
@@ -25,6 +27,40 @@ router.get("/:id", async (req: Request, res: Response) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+// get user following
+router.get("/following/:id", async (req : Request, res : Response) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId).populate("following");
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const userFollowings = user.following;
+    res.status(200).json(userFollowings);
+  } catch (err) {
+    console.error(`Error fetching users: ${err}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+})
+
+// get recommended users
+router.get("/recommended/:id", async (req : Request, res : Response) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId).populate("following");
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const userFollowings : any = user.following;
+    userFollowings.push(userId);
+    const recommendedUsers = await User.find({ _id: { $nin: userFollowings } });
+    res.status(200).json(recommendedUsers);
+  } catch (err) {
+    console.error(`Error fetching users: ${err}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+})
 
 
 
