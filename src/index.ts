04f74e5f15f1303144c2f60c795b752ece2736a7
Arjun Sharma <arjunsharma6622@gmail.com>
2024-07-27 20:13:55 +0530

    changed the recommended users api

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,12 @@ import dotenv from "dotenv";
 import express from "express";
 import { User } from "./models/user";
 import { Venue } from "./models/venue";
-import session from "express-session"
-import passport from "passport"
-import { Strategy as OAuth2Strategy} from "passport-google-oauth2"
+import authRoutes from "./routes/auth";
 import conversationRoutes from "./routes/conversation";
 import imageRoutes from "./routes/images";
 import postRoutes from "./routes/posts";
 import productRoutes from "./routes/product";
 import userRoutes from "./routes/user";
-import authRoutes from "./routes/auth";
 import venueRoutes from "./routes/venue";
 
 import { checkNameAvailability, connectDB } from "./utils/utils";
@@ -19,7 +16,7 @@ import { checkNameAvailability, connectDB } from "./utils/utils";
 const app = express();
 
 const corsOptions: CorsOptions = {
-  origin: ["http://localhost:3000", "https://notdoneyet.vercel.app", ""],
+  origin: ["http://localhost:3000", "https://notdoneyet.vercel.app", "*"],
   credentials: true,
   optionsSuccessStatus: 200,
 };
@@ -29,55 +26,55 @@ app.use(express.json());
 dotenv.config();
 
 //set up session
-app.use(session({
-  secret: process.env.SESSION_SECRET_KEY as string,
-  resave: false,
-  saveUninitialized: true,
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(session({
+//   secret: process.env.SESSION_SECRET_KEY as string,
+//   resave: false,
+//   saveUninitialized: true,
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session())
 
-passport.use(
-  new OAuth2Strategy({
-    clientID: process.env.GOOGLE_CLIENT_ID as string,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    callbackURL: "/auth/callback/google",
-    scope: ["profile", "email"]
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try{
-      console.log(profile)
-      let user = await User.findOne({googleId: profile.id})
-      if(!user){
-        user = new User({
-          googleId: profile.id,
-          name: profile.displayName,
-          userName: profile.email.split("@")[0],
-          email: profile.email,
-          image: profile.picture
-        })
-        await user.save()
-      }
-      return done(null, user)
-    }catch(error){
-      return done(error, false)
-    }
-  })
-)
+// passport.use(
+//   new OAuth2Strategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID as string,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     callbackURL: "/auth/callback/google",
+//     scope: ["profile", "email"]
+//   },
+//   async (accessToken, refreshToken, profile, done) => {
+//     try{
+//       console.log(profile)
+//       let user = await User.findOne({googleId: profile.id})
+//       if(!user){
+//         user = new User({
+//           googleId: profile.id,
+//           name: profile.displayName,
+//           userName: profile.email.split("@")[0],
+//           email: profile.email,
+//           image: profile.picture
+//         })
+//         await user.save()
+//       }
+//       return done(null, user)
+//     }catch(error){
+//       return done(error, false)
+//     }
+//   })
+// )
 
-passport.serializeUser((user, done) => {
-  done(null, user as any)
-})
-passport.deserializeUser((user, done) => {
-  done(null, user as any)
-})
+// passport.serializeUser((user, done) => {
+//   done(null, user as any)
+// })
+// passport.deserializeUser((user, done) => {
+//   done(null, user as any)
+// })
 
 // initialize googleAuth Login
-app.get("/auth/google", passport.authenticate("google", {scope: ["profile", "email"]}))
-app.get("/auth/callback/google", passport.authenticate("google", {
-  successRedirect : "http://localhost:3000/dashboard",
-  failureRedirect : "http://localhost:3000/login"
-}))
+// app.get("/auth/google", passport.authenticate("google", {scope: ["profile", "email"]}))
+// app.get("/auth/callback/google", passport.authenticate("google", {
+//   successRedirect : "http://localhost:3000/dashboard",
+//   failureRedirect : "http://localhost:3000/login"
+// }))
 
 const PORT = process.env.PORT || 8000;
 
